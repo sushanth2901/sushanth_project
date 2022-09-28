13ab9dd5a5f9a03583221f6ed199de130b8da457
sushanth2901 <75194546+sushanth2901@users.noreply.github.com>
2022-09-28 20:08:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 
 let homeContent = "";
 let projectContent = "";
+let registrationContent="";
 
 fs.readFile("home.html", (err, home) => {
   if (err) {
@@ -17,19 +18,33 @@ fs.readFile("project.html", (err, project) => {
   }
   projectContent = project;
 });
-http
-  .createServer((request, response) => {
+
+fs.readFile("registration.html",(err,reg) => {
+  if (err){
+    throw err;
+  }
+  registrationContent=reg;
+});
+
+let args=require("minimist")(process.argv.slice(2));
+
+http.createServer((request, response) => {
     let url = request.url;
     response.writeHeader(200, { "Content-Type": "text/html" });
     switch (url) {
-      case "/project":
-        response.write(projectContent);
-        response.end();
-        break;
-      default:
-        response.write(homeContent);
-        response.end();
-        break;
+        case "/project":
+          response.write(projectContent);
+          response.end();
+          break;
+        case "/registration":
+          response.write(registrationContent);
+          response.end();
+          break;
+
+        default:
+          response.write(homeContent);
+          response.end();
+          break;
     }
   })
-  .listen(5000);
+  .listen(args["port"]);
