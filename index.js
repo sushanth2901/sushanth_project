f53f79d01b0996deb4c8c0bb00ce93cfe6583900
sushanth2901 <75194546+sushanth2901@users.noreply.github.com>
2022-09-28 19:25:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
 const http = require("http");
 const fs = require("fs");
-fs.readFile("home.html", (err, home) => {
-  console.log(home.toString());
-});
-fs.readFile("home.html", (err, home) => {
-  if (err) {
-    throw err;
-  }
-  http
-    .createServer((request, response) => {
-      response.writeHeader(200, { "Content-Type": "text/html" });
-      response.write(home);
-      response.end();
-    })
-    .listen(3000);
-});
-const http = require("http");
-const fs = require("fs");
 
 let homeContent = "";
 let projectContent = "";
@@ -34,3 +17,19 @@ fs.readFile("project.html", (err, project) => {
   }
   projectContent = project;
 });
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(3000);
